00f0597f98e0bfadbd49b9d4aaacb6fe988d463f
lucky <mg@wefra.life>
2020-04-17 09:49:23 +0200

    adjusting script to be a bit more verbose

--- docker/update-all.sh
+++ docker/update-all.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
-DOCKER_BASE_DIR="/PATH/TO/DOCKER/DIR"
+DOCKER_BASE_DIR="/PATH_TO_DOCKER_FOLDER"
 cd ${DOCKER_BASE_DIR}
 for D in *; do
     if [ -d "${D}" ]; then
-        cd ${D} && docker-compose pull && docker-compose up -d && cd ..
+        cd ${D}
+        echo "I am in $(pwd)"
+        if [ -f "./docker-compose.yaml" ]; then
+            echo "I found a docker-compose.yaml, updating ..."
+            docker-compose pull && docker-compose up -d && cd ..
+        else
+            echo "No docker-compose.yaml found, stepping back"
+            cd ..
+        fi
     fi
 done
